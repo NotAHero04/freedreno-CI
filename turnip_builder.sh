6719447d7b7a8cad498e2a996ccae5f37a767a0d
NotAHero04 <84689840+NotAHero04@users.noreply.github.com>
2023-07-25 09:14:16 +0700

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -136,4 +136,3 @@ if ! [ -a $workdir/turnip-23.2.0-T-Alpha_MrPurple.adpkg.zip ];
 	then echo -e "$red-Packing failed!$nocolor" && exit 1
 	else echo -e "$green-All done, you can take your module from here;$nocolor" && echo $workdir/turnip-23.0.0-T-Alpha_MrPurple.adpkg.zip
 fi
-find
